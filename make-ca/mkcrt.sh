3400228a5da303afa83e0ad9577c1b8cab5b7904
Uplusware <uplusware@gmail.com>
2016-05-30 15:50:22 +0800

    1.6.03
    1.6.03

--- make-ca/mkcrt.sh
+++ make-ca/mkcrt.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
-touch index.txt serial
-echo 01 > serial
-mkdir private
-mkdir certs
-openssl genrsa -aes256 -out private/cakey.pem 1024
-openssl req -new -key private/cakey.pem -out private/ca.csr -subj "/C=CN/ST=BJ/L=BJ/O=uplusware/OU=uplusware/CN=localhost"
-openssl x509 -req -days 365 -sha1 -extensions v3_ca -signkey private/cakey.pem -in private/ca.csr -out certs/ca.cer
+_PASSWORD_=123456
+_COUNTRY_="CN"
+_COMMON_NAME_="localhost"
+_ORG_="Uplusware"
+_ORG_UNIT_=
 
-openssl genrsa -aes256 -out private/server-key.pem 1024
-openssl req -new -key private/server-key.pem -out private/server.csr -subj "/C=CN/ST=BJ/L=BJ/O=uplusware/OU=uplusware/CN=localhost"
-openssl x509 -req -days 365 -sha1 -extensions v3_req -CA certs/ca.cer -CAkey private/cakey.pem -CAserial ca.srl -CAcreateserial -in private/server.csr -out certs/server.cer
+test -x index.txt || touch index.txt
+test -x serial || ( touch serial && echo 01 > serial )
+test -x private || mkdir private
+test -x certs || mkdir certs
+openssl genrsa -aes256 -passout pass:${_PASSWORD_} -out private/cakey.pem 1024
 
-openssl genrsa -aes256 -out private/client-key.pem 1024
-openssl req -new -key private/client-key.pem -out private/client.csr -subj "/C=CN/ST=BJ/L=BJ/O=uplusware/OU=uplusware/CN=localhost"
-openssl x509 -req -days 365 -sha1 -extensions v3_req -CA certs/ca.cer -CAkey private/cakey.pem -CAserial ca.srl -in private/client.csr -out certs/client.cer
+openssl req -new -key private/cakey.pem  -passin pass:${_PASSWORD_} -passout pass:${_PASSWORD_} -out private/ca.csr -subj "/C="${_COUNTRY_}"/ST=/L=/O="${_ORG_}"/OU="${_ORG_UNIT_}"/CN="${_COMMON_NAME_}
+openssl x509 -req -days 365 -sha1 -extensions v3_ca -signkey private/cakey.pem -in private/ca.csr -out certs/ca.cer -passin pass:${_PASSWORD_}
 
-openssl pkcs12 -export -clcerts -inkey private/client-key.pem -in certs/client.cer -out certs/client.p12
-openssl pkcs12 -export -clcerts -inkey private/server-key.pem -in certs/server.cer -out certs/server.p12
+openssl genrsa -aes256 -passout pass:${_PASSWORD_} -out private/server-key.pem 1024
+openssl req -new -key private/server-key.pem -passin pass:${_PASSWORD_} -passout pass:${_PASSWORD_} -out private/server.csr -subj "/C="${_COUNTRY_}"/ST=/L=/O="${_ORG_}"/OU="${_ORG_UNIT_}"/CN="${_COMMON_NAME_}
+
+openssl x509 -req -days 365 -sha1 -extensions v3_req -CA certs/ca.cer -CAkey private/cakey.pem -CAserial ca.srl -CAcreateserial -in private/server.csr -passin pass:${_PASSWORD_} -out certs/server.cer
+
+openssl genrsa -aes256 -passout pass:${_PASSWORD_} -out private/client-key.pem 1024
+openssl req -new -key private/client-key.pem -passin pass:${_PASSWORD_} -passout pass:${_PASSWORD_} -out private/client.csr -subj "/C="${_COUNTRY_}"/ST=/L=/O="${_ORG_}"/OU="${_ORG_UNIT_}"/CN="${_COMMON_NAME_}
+
+openssl x509 -req -days 365 -sha1 -extensions v3_req -CA certs/ca.cer -CAkey private/cakey.pem -CAserial ca.srl -CAcreateserial -in private/client.csr -passin pass:${_PASSWORD_} -out certs/client.cer
+
+openssl pkcs12 -export -clcerts -inkey private/client-key.pem -in certs/client.cer -passout pass:${_PASSWORD_} -passin pass:${_PASSWORD_} -out certs/client.p12
+openssl pkcs12 -export -clcerts -inkey private/server-key.pem -in certs/server.cer -passout pass:${_PASSWORD_} -passin pass:${_PASSWORD_} -out certs/server.p12
