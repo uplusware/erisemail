fd1722dde3d2fc8e29fb9b03ec8fdc588ddee8c5
Uplusware <uplusware@gmail.com>
2016-12-08 21:23:59 +0800

    1.6.08
    1.6.08

--- bld.sh
+++ bld.sh
@@ -1,3 +1,8 @@
+cd src/
+make clean
+make
+cd ..
+chmod a+x ./release.sh
 ./release.sh 1.6.08 utf8 ubuntu
 sudo /etc/init.d/erisemail stop
 sudo ./ubuntu-erisemail-bin-en-utf8-x86_64-linux/install.sh
--- release.sh
+++ release.sh
@@ -5,7 +5,7 @@ then
 	echo $1
 else
 	echo "release 1.6.08 utf8 centos7"
-	exit 1	
+	exit 1
 fi
 
 path=$(dirname $0)
@@ -21,31 +21,6 @@ m=`uname -m`
 if uname -o | grep -i linux;
 then
 	o=linux
-	cd src/
-	make clean
-	make
-	cd ..
-elif uname -o | grep -i solaris;
-then
-	o=solaris-`isainfo -b`bit
-	cd src/
-	gmake clean
-	gmake SOLARIS=1
-	cd ..
-elif uname -o | grep -i freebsd;
-then
-	o=freebsd
-	cd src/
-	make clean
-	make FREEBSD=1
-	cd ..
-elif uname -o | grep -i cygwin;
-then
-	o=cygwin
-	cd src/
-	make clean
-	make CYGWIN=1
-	cd ..
 fi
 
 ##############################################################################
