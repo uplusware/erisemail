2985004e69290a4b780459b7e4cf9858ae10fee1
Uplusware <uplusware@gmail.com>
2018-06-25 12:54:50 +0800

    1.6.10
    1.6.10

---
+++ test/build_release_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
+cd ${SCRIPT_DIR}/src/
+#make clean
+make
+cd ${SCRIPT_DIR}
+chmod a+x ${SCRIPT_DIR}/release.sh
+${SCRIPT_DIR}/release.sh 1.6.10 utf8 ubuntu
+sudo /etc/init.d/erisemail stop
+sudo ${SCRIPT_DIR}/ubuntu-erisemail-bin-cn-utf8-x86_64-linux/install.sh
+sudo /etc/init.d/erisemail start
