fa9886d0d7f5b843b371aec0c5cf4a6b6280d673
Uplusware <uplusware@gmail.com>
2019-05-09 10:48:36 +0800

    1.6.10
    1.6.10

--- src/ldap_message.cpp
+++ src/ldap_message.cpp
@@ -12,7 +12,7 @@ int LDAPMessage::parse(const unsigned char* msg_buf, unsigned int msg_len)
     asn1_ber_tag message_id_ber_tag;
     message_id_ber_tag.payload = msg_buf[0];
     
-    printf("encode: %d\n", message_id_ber_tag.tag.encode);
+   //printf("encode: %d\n", message_id_ber_tag.tag.encode);
     if(message_id_ber_tag.tag.type != ASN_1_TYPE_UNIVERSAL || message_id_ber_tag.tag.val != ASN_1_INTEGER)
     {
         return -1;
@@ -29,11 +29,11 @@ int LDAPMessage::parse(const unsigned char* msg_buf, unsigned int msg_len)
         m_messageID += msg_buf[1 + len_bytes + x];
     }
     
-    printf("%d %d %d\n", data_len, len_bytes, m_messageID);
+   //printf("%d %d %d\n", data_len, len_bytes, m_messageID);
     
     asn1_ber_tag protocol_op_ber_tag;
     protocol_op_ber_tag.payload = msg_buf[1 + len_bytes + data_len];
-    printf("payload: %x, encode: %d\n", protocol_op_ber_tag.payload, protocol_op_ber_tag.tag.encode);
+   //printf("payload: %x, encode: %d\n", protocol_op_ber_tag.payload, protocol_op_ber_tag.tag.encode);
     
     if(protocol_op_ber_tag.tag.type != ASN_1_TYPE_APPLICATION || protocol_op_ber_tag.tag.encode != ASN_1_ENCODE_CONSTRUCTED)
     {
@@ -45,7 +45,7 @@ int LDAPMessage::parse(const unsigned char* msg_buf, unsigned int msg_len)
     unsigned int len_bytes2;
     int data_len2 = get_ber_val_len(&msg_buf[1 + len_bytes + data_len + 1], len_bytes2);
     
-    printf("%x %x, m_protocolOp: %d\n", data_len2, len_bytes2, m_protocolOp);
+   //printf("%x %x, m_protocolOp: %d\n", data_len2, len_bytes2, m_protocolOp);
      
      
     if(m_protocolOp == LDAP_PROTO_OPS_BIND_REQUEST)
@@ -90,11 +90,11 @@ int LDAPbindRequest::parse(const unsigned char* msg_buf, unsigned int msg_len)
         m_version += msg_buf[1 + len_bytes + x];
     }
     
-    printf("LDAPbindRequest: %d %d %d\n", data_len, len_bytes, m_version);
+   //printf("LDAPbindRequest: %d %d %d\n", data_len, len_bytes, m_version);
     
     asn1_ber_tag name_ber_tag;
     name_ber_tag.payload = msg_buf[1 + len_bytes + data_len];
-    printf("payload: %x, encode: %d\n", name_ber_tag.payload, name_ber_tag.tag.encode);
+   //printf("payload: %x, encode: %d\n", name_ber_tag.payload, name_ber_tag.tag.encode);
     
     if(name_ber_tag.tag.type != ASN_1_TYPE_UNIVERSAL || name_ber_tag.tag.val != ASN_1_OCTET_STRING)
     {
@@ -110,12 +110,12 @@ int LDAPbindRequest::parse(const unsigned char* msg_buf, unsigned int msg_len)
     
     common_name[data_len2] = '\0';
     
-    printf("%s\n", common_name);
+   //printf("%s\n", common_name);
     
     
     asn1_ber_tag password_ber_tag;
     password_ber_tag.payload = msg_buf[1 + len_bytes + data_len+ 1 + len_bytes2 + data_len2];
-    printf("payload: %x, encode: %d\n", password_ber_tag.payload, password_ber_tag.tag.encode);
+   //printf("payload: %x, encode: %d\n", password_ber_tag.payload, password_ber_tag.tag.encode);
     
     if(password_ber_tag.tag.type != ASN_1_TYPE_CONTEXT_SPECIFIC)
     {
@@ -131,7 +131,7 @@ int LDAPbindRequest::parse(const unsigned char* msg_buf, unsigned int msg_len)
     
     password[data_len3] = '\0';
     
-    printf("%s\n", password);
+   //printf("%s\n", password);
     
     free(common_name);
     free(password);
