4a68aa1a5f5f8fa376d725beae0bd5d39df36e1f
Uplusware <uplusware@gmail.com>
2021-12-09 15:50:20 +0800

    1.6.13
    1.6.13

--- test/build_release_all.sh
+++ test/build_release_all.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
-cd ${SCRIPT_DIR}/src/
+VERSION=1.6.13
+ENCODING=utf8
+OS=centos8
 
+SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
+cd ${SCRIPT_DIR}/../src/
 make clean
 make
 cd ${SCRIPT_DIR}
 chmod a+x ${SCRIPT_DIR}/release.sh
-${SCRIPT_DIR}/release.sh 1.6.10 utf8 ubuntu18lts
+${SCRIPT_DIR}/release.sh ${VERSION} ${ENCODING} ${OS}
 
-cd ${SCRIPT_DIR}/src/
+cd ${SCRIPT_DIR}/../src/
 make clean
 make DIST=1
 cd ${SCRIPT_DIR}
 chmod a+x ${SCRIPT_DIR}/release.sh
-${SCRIPT_DIR}/release.sh 1.6.10-dist utf8 ubuntu18lts
+${SCRIPT_DIR}/release.sh ${VERSION}-dist ${ENCODING} ${OS}
+cd ${SCRIPT_DIR}
