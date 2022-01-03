88b4ff53db4f8448a9fe5bb075fbf4f5882f0632
Uplusware <uplusware@gmail.com>
2022-01-03 09:55:04 +0800

    1.6.13
    1.6.13

--- webmail/js/man_home.js
+++ webmail/js/man_home.js
@@ -23,7 +23,41 @@ function change_tab(tabname) {
 }
 
 $(document).ready(function () {
-    
+    $('#man_status').click(function () {
+        window.parent.childframe.location.href='man_status.html';
+    });
+
+    $('#man_levels').click(function () {
+        window.parent.childframe.location.href='man_levels.html';
+    });
+
+    $('#man_mails').click(function () {
+        window.parent.childframe.location.href='man_mails.html';
+    });
+
+    $('#man_users').click(function () {
+        window.parent.childframe.location.href='man_users.html';
+    });
+
+    $('#man_groups').click(function () {
+        window.parent.childframe.location.href='man_groups.html';
+    });
+
+    $('#man_access').click(function () {
+        window.parent.childframe.location.href='man_access.html';
+    });
+
+    $('#man_logs').click(function () {
+        window.parent.childframe.location.href='man_logs.html';
+    });
+
+    $('#man_cluster').click(function () {
+        window.parent.childframe.location.href='man_cluster.html';
+    });
+
+    $('#man_logout').click(function () {
+        logout();
+    });
 });
 
 $(window).on('unload',function(){
